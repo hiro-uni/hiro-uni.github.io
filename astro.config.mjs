ad8cdb7283dd6015a2b82cfc0fc21886a1ce50fb
NAKAOKU Takahiro <giraffe_rakugaki@yahoo.co.jp>
2026-07-20 21:43:03 +0900

    SEO対策関連の設定を修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +16,5 @@ export default defineConfig({
     rehypePlugins: [],
   },
 
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()]
 });
